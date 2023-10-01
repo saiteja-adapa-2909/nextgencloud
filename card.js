b710ee0b4e09ee757b62f41c1abb7fa10ec7ac80
saiteja-adapa-2909 <98104890+saiteja-adapa-2909@users.noreply.github.com>
2023-10-01 15:18:39 +0530

    Add files via upload

---
+++ card.js
@@ -0,0 +1,309 @@
+let memberCard = document.getElementById("memberCard");
+let memberFront = document.getElementById("memberFront");
+let memberBack = document.getElementById("memberBack");
+
+memberCard.onclick = function(){
+    memberFront.classList.toggle("card-front1");
+    memberFront.classList.toggle("card-front");
+    memberBack.classList.toggle("card-back1");
+    memberBack.classList.toggle("card-back");
+};
+
+/*let memberCard1 = document.getElementById("memberCard1");
+let memberFront1 = document.getElementById("memberFront1");
+let memberBack1 = document.getElementById("memberBack1");
+
+memberCard1.onclick = function(){
+    memberFront1.classList.toggle("card-front1");
+    memberFront1.classList.toggle("card-front");
+    memberBack1.classList.toggle("card-back1");
+    memberBack1.classList.toggle("card-back");
+}; */
+
+let memberCardColead1 = document.getElementById("memberCardColead1");
+let memberFrontColead1 = document.getElementById("memberFrontColead1");
+let memberBackColead1 = document.getElementById("memberBackColead1");
+
+memberCardColead1.onclick = function(){
+    memberFrontColead1.classList.toggle("card-front1");
+    memberFrontColead1.classList.toggle("card-front");
+    memberBackColead1.classList.toggle("card-back1");
+    memberBackColead1.classList.toggle("card-back");
+};
+
+
+let memberCardColead2 = document.getElementById("memberCardColead2");
+let memberFrontColead2 = document.getElementById("memberFrontColead2");
+let memberBackColead2 = document.getElementById("memberBackColead2");
+
+memberCardColead2.onclick = function(){
+    memberFrontColead2.classList.toggle("card-front1");
+    memberFrontColead2.classList.toggle("card-front");
+    memberBackColead2.classList.toggle("card-back1");
+    memberBackColead2.classList.toggle("card-back");
+};
+
+let memberCardMember1 = document.getElementById("memberCardMember1");
+let memberFrontMember1 = document.getElementById("memberFrontMember1");
+let memberBackMember1 = document.getElementById("memberBackMember1");
+
+memberCardMember1.onclick = function(){
+    memberFrontMember1.classList.toggle("card-front1");
+    memberFrontMember1.classList.toggle("card-front");
+    memberBackMember1.classList.toggle("card-back1");
+    memberBackMember1.classList.toggle("card-back");
+};
+
+let memberCardMember2 = document.getElementById("memberCardMember2");
+let memberFrontMember2 = document.getElementById("memberFrontMember2");
+let memberBackMember2 = document.getElementById("memberBackMember2");
+
+memberCardMember2.onclick = function(){
+    memberFrontMember2.classList.toggle("card-front1");
+    memberFrontMember2.classList.toggle("card-front");
+    memberBackMember2.classList.toggle("card-back1");
+    memberBackMember2.classList.toggle("card-back");
+};
+
+let memberCardMember3 = document.getElementById("memberCardMember3");
+let memberFrontMember3 = document.getElementById("memberFrontMember3");
+let memberBackMember3 = document.getElementById("memberBackMember3");
+
+memberCardMember3.onclick = function(){
+    memberFrontMember3.classList.toggle("card-front1");
+    memberFrontMember3.classList.toggle("card-front");
+    memberBackMember3.classList.toggle("card-back1");
+    memberBackMember3.classList.toggle("card-back");
+};
+
+let memberCardMember4 = document.getElementById("memberCardMember4");
+let memberFrontMember4 = document.getElementById("memberFrontMember4");
+let memberBackMember4 = document.getElementById("memberBackMember4");
+
+memberCardMember4.onclick = function(){
+    memberFrontMember4.classList.toggle("card-front1");
+    memberFrontMember4.classList.toggle("card-front");
+    memberBackMember4.classList.toggle("card-back1");
+    memberBackMember4.classList.toggle("card-back");
+};
+
+let memberCardMember5 = document.getElementById("memberCardMember5");
+let memberFrontMember5 = document.getElementById("memberFrontMember5");
+let memberBackMember5 = document.getElementById("memberBackMember5");
+
+memberCardMember5.onclick = function(){
+    memberFrontMember5.classList.toggle("card-front1");
+    memberFrontMember5.classList.toggle("card-front");
+    memberBackMember5.classList.toggle("card-back1");
+    memberBackMember5.classList.toggle("card-back");
+};
+
+let memberCardMember6 = document.getElementById("memberCardMember6");
+let memberFrontMember6 = document.getElementById("memberFrontMember6");
+let memberBackMember6 = document.getElementById("memberBackMember6");
+
+memberCardMember6.onclick = function(){
+    memberFrontMember6.classList.toggle("card-front1");
+    memberFrontMember6.classList.toggle("card-front");
+    memberBackMember6.classList.toggle("card-back1");
+    memberBackMember6.classList.toggle("card-back");
+};
+
+let memberCardMember7 = document.getElementById("memberCardMember7");
+let memberFrontMember7 = document.getElementById("memberFrontMember7");
+let memberBackMember7 = document.getElementById("memberBackMember7");
+
+memberCardMember7.onclick = function(){
+    memberFrontMember7.classList.toggle("card-front1");
+    memberFrontMember7.classList.toggle("card-front");
+    memberBackMember7.classList.toggle("card-back1");
+    memberBackMember7.classList.toggle("card-back");
+};
+
+let memberCardMember8 = document.getElementById("memberCardMember8");
+let memberFrontMember8 = document.getElementById("memberFrontMember8");
+let memberBackMember8 = document.getElementById("memberBackMember8");
+
+memberCardMember8.onclick = function(){
+    memberFrontMember8.classList.toggle("card-front1");
+    memberFrontMember8.classList.toggle("card-front");
+    memberBackMember8.classList.toggle("card-back1");
+    memberBackMember8.classList.toggle("card-back");
+};
+
+let memberCardMember9 = document.getElementById("memberCardMember9");
+let memberFrontMember9 = document.getElementById("memberFrontMember9");
+let memberBackMember9 = document.getElementById("memberBackMember9");
+
+memberCardMember9.onclick = function(){
+    memberFrontMember9.classList.toggle("card-front1");
+    memberFrontMember9.classList.toggle("card-front");
+    memberBackMember9.classList.toggle("card-back1");
+    memberBackMember9.classList.toggle("card-back");
+};
+
+let memberCardMember10 = document.getElementById("memberCardMember10");
+let memberFrontMember10 = document.getElementById("memberFrontMember10");
+let memberBackMember10 = document.getElementById("memberBackMember10");
+
+memberCardMember10.onclick = function(){
+    memberFrontMember10.classList.toggle("card-front1");
+    memberFrontMember10.classList.toggle("card-front");
+    memberBackMember10.classList.toggle("card-back1");
+    memberBackMember10.classList.toggle("card-back");
+};
+
+let memberCardMember11 = document.getElementById("memberCardMember11");
+let memberFrontMember11 = document.getElementById("memberFrontMember11");
+let memberBackMember11 = document.getElementById("memberBackMember11");
+
+memberCardMember11.onclick = function(){
+    memberFrontMember11.classList.toggle("card-front1");
+    memberFrontMember11.classList.toggle("card-front");
+    memberBackMember11.classList.toggle("card-back1");
+    memberBackMember11.classList.toggle("card-back");
+};
+
+let memberCardMember12 = document.getElementById("memberCardMember12");
+let memberFrontMember12 = document.getElementById("memberFrontMember12");
+let memberBackMember12 = document.getElementById("memberBackMember12");
+
+memberCardMember12.onclick = function(){
+    memberFrontMember12.classList.toggle("card-front1");
+    memberFrontMember12.classList.toggle("card-front");
+    memberBackMember12.classList.toggle("card-back1");
+    memberBackMember12.classList.toggle("card-back");
+};
+
+let memberCardMember13 = document.getElementById("memberCardMember13");
+let memberFrontMember13 = document.getElementById("memberFrontMember13");
+let memberBackMember13 = document.getElementById("memberBackMember13");
+
+memberCardMember13.onclick = function(){
+    memberFrontMember13.classList.toggle("card-front1");
+    memberFrontMember13.classList.toggle("card-front");
+    memberBackMember13.classList.toggle("card-back1");
+    memberBackMember13.classList.toggle("card-back");
+};
+
+let memberCardMember14 = document.getElementById("memberCardMember14");
+let memberFrontMember14 = document.getElementById("memberFrontMember14");
+let memberBackMember14 = document.getElementById("memberBackMember14");
+
+memberCardMember14.onclick = function(){
+    memberFrontMember14.classList.toggle("card-front1");
+    memberFrontMember14.classList.toggle("card-front");
+    memberBackMember14.classList.toggle("card-back1");
+    memberBackMember14.classList.toggle("card-back");
+};
+
+let memberCardMember15 = document.getElementById("memberCardMember15");
+let memberFrontMember15 = document.getElementById("memberFrontMember15");
+let memberBackMember15 = document.getElementById("memberBackMember15");
+
+memberCardMember15.onclick = function(){
+    memberFrontMember15.classList.toggle("card-front1");
+    memberFrontMember15.classList.toggle("card-front");
+    memberBackMember15.classList.toggle("card-back1");
+    memberBackMember15.classList.toggle("card-back");
+};
+
+let memberCardMember16 = document.getElementById("memberCardMember16");
+let memberFrontMember16 = document.getElementById("memberFrontMember16");
+let memberBackMember16 = document.getElementById("memberBackMember16");
+
+memberCardMember16.onclick = function(){
+    memberFrontMember16.classList.toggle("card-front1");
+    memberFrontMember16.classList.toggle("card-front");
+    memberBackMember16.classList.toggle("card-back1");
+    memberBackMember16.classList.toggle("card-back");
+};
+
+let memberCardMember17 = document.getElementById("memberCardMember17");
+let memberFrontMember17 = document.getElementById("memberFrontMember17");
+let memberBackMember17 = document.getElementById("memberBackMember17");
+
+memberCardMember17.onclick = function(){
+    memberFrontMember17.classList.toggle("card-front1");
+    memberFrontMember17.classList.toggle("card-front");
+    memberBackMember17.classList.toggle("card-back1");
+    memberBackMember17.classList.toggle("card-back");
+};
+
+let memberCardMember18 = document.getElementById("memberCardMember18");
+let memberFrontMember18 = document.getElementById("memberFrontMember18");
+let memberBackMember18 = document.getElementById("memberBackMember18");
+
+memberCardMember18.onclick = function(){
+    memberFrontMember18.classList.toggle("card-front1");
+    memberFrontMember18.classList.toggle("card-front");
+    memberBackMember18.classList.toggle("card-back1");
+    memberBackMember18.classList.toggle("card-back");
+};
+
+let memberCardMember19 = document.getElementById("memberCardMember19");
+let memberFrontMember19 = document.getElementById("memberFrontMember19");
+let memberBackMember19 = document.getElementById("memberBackMember19");
+
+memberCardMember19.onclick = function(){
+    memberFrontMember19.classList.toggle("card-front1");
+    memberFrontMember19.classList.toggle("card-front");
+    memberBackMember19.classList.toggle("card-back1");
+    memberBackMember19.classList.toggle("card-back");
+};
+
+let memberCardMember20 = document.getElementById("memberCardMember20");
+let memberFrontMember20 = document.getElementById("memberFrontMember20");
+let memberBackMember20 = document.getElementById("memberBackMember20");
+
+memberCardMember20.onclick = function(){
+    memberFrontMember20.classList.toggle("card-front1");
+    memberFrontMember20.classList.toggle("card-front");
+    memberBackMember20.classList.toggle("card-back1");
+    memberBackMember20.classList.toggle("card-back");
+};
+
+let memberCardMember21 = document.getElementById("memberCardMember21");
+let memberFrontMember21 = document.getElementById("memberFrontMember21");
+let memberBackMember21 = document.getElementById("memberBackMember21");
+
+memberCardMember21.onclick = function(){
+    memberFrontMember21.classList.toggle("card-front1");
+    memberFrontMember21.classList.toggle("card-front");
+    memberBackMember21.classList.toggle("card-back1");
+    memberBackMember21.classList.toggle("card-back");
+};
+
+let memberCardMember22 = document.getElementById("memberCardMember22");
+let memberFrontMember22 = document.getElementById("memberFrontMember22");
+let memberBackMember22 = document.getElementById("memberBackMember22");
+
+memberCardMember22.onclick = function(){
+    memberFrontMember22.classList.toggle("card-front1");
+    memberFrontMember22.classList.toggle("card-front");
+    memberBackMember22.classList.toggle("card-back1");
+    memberBackMember22.classList.toggle("card-back");
+};
+
+let memberCardMember23 = document.getElementById("memberCardMember23");
+let memberFrontMember23 = document.getElementById("memberFrontMember23");
+let memberBackMember23 = document.getElementById("memberBackMember23");
+
+memberCardMember23.onclick = function(){
+    memberFrontMember23.classList.toggle("card-front1");
+    memberFrontMember23.classList.toggle("card-front");
+    memberBackMember23.classList.toggle("card-back1");
+    memberBackMember23.classList.toggle("card-back");
+};
+
+let memberCardMember24 = document.getElementById("memberCardMember24");
+let memberFrontMember24 = document.getElementById("memberFrontMember24");
+let memberBackMember24 = document.getElementById("memberBackMember24");
+
+memberCardMember24.onclick = function(){
+    memberFrontMember24.classList.toggle("card-front1");
+    memberFrontMember24.classList.toggle("card-front");
+    memberBackMember24.classList.toggle("card-back1");
+    memberBackMember24.classList.toggle("card-back");
+};
+
